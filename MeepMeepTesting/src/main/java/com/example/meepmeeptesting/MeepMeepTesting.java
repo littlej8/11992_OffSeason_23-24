2b2e24717218a6c000a5c2c8750c768780ed24e1
littlej8 <jacksontravis2008@gmail.com>
2024-03-14 22:10:00 -0500

    best auto concept

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,9 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -12,24 +15,36 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(160, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(14.2, -62, 0))
+        Action initial = myBot.getDrive().actionBuilder(new Pose2d(14.2, -62, 0))
                 .splineToConstantHeading(new Vector2d(50, -30), 0)
                 .waitSeconds(1)
-                .lineToXSplineHeading(30, Math.toRadians(180))
+                .lineToXConstantHeading(32)
                 .waitSeconds(1)
-                .lineToXConstantHeading(35)
-                .splineToConstantHeading(new Vector2d(30, -60), Math.toRadians(180))
-                .lineToXConstantHeading(-50)
-                .splineToConstantHeading(new Vector2d(-60, -11), Math.toRadians(180))
+                //.lineToXConstantHeading(35)
+                .splineToConstantHeading(new Vector2d(30, -12), 0)
+                .lineToXConstantHeading(-60, null, new ProfileAccelConstraint(-120, 120))
                 .waitSeconds(1)
-                .splineToConstantHeading(new Vector2d(-50, -60), Math.toRadians(180))
-                .lineToXConstantHeading(35)
-                .splineToSplineHeading(new Pose2d(50, -30, Math.toRadians(180)), 0)
+                .lineToXConstantHeading(35, null, new ProfileAccelConstraint(-120, 120))
+                .splineToConstantHeading(new Vector2d(50, -30), 0)
                 .waitSeconds(1)
-                .build());
+                .build();
+
+        Action cycle = myBot.getDrive().actionBuilder(new Pose2d(50, -30, 0))
+                .lineToXConstantHeading(48)
+                .splineToConstantHeading(new Vector2d(30, -12), 0)
+                .lineToXConstantHeading(-60, null, new ProfileAccelConstraint(-120, 120))
+                .waitSeconds(1)
+                .lineToXConstantHeading(35, null, new ProfileAccelConstraint(-120, 120))
+                .splineToConstantHeading(new Vector2d(50, -30), 0)
+                .waitSeconds(1)
+                .build();
+
+        SequentialAction program = new SequentialAction(initial, cycle, cycle);
+
+        myBot.runAction(program);
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
