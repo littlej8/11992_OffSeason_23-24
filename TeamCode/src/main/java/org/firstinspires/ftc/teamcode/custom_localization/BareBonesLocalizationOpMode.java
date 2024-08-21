111209e1576f2edb6ed1f6159c84742dd6eea542
littlej8 <95560628+littlej8@users.noreply.github.com>
2024-08-20 22:37:42 -0500

    Update BareBonesLocalizationOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/custom_localization/BareBonesLocalizationOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/custom_localization/BareBonesLocalizationOpMode.java
@@ -1,16 +1,13 @@
-package org.firstinspires.ftc.teamcode.custom_localization;
+package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.hardware.lynx.LynxModule;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,13 +20,16 @@ public class BareBonesLocalizationOpMode extends LinearOpMode {
     IMU imu;
 
     public static double STRAFE_MULT = 1.0;
+    public static double TICKS_PER_REV = 1120;
+    public static double WHEEL_DIAMETER = 4;
+    public static double IN_PER_TICK = (WHEEL_DIAMETER * Math.PI) / TICKS_PER_REV;
 
     @Override
     public void runOpMode() {
         for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
             module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
         }
-        
+
         for (String motor : motorNames)
             motors.add(hardwareMap.get(DcMotorEx.class, motor));
 
@@ -57,17 +57,36 @@ public class BareBonesLocalizationOpMode extends LinearOpMode {
 
         while (!isStopRequested()) {
             heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-          
+
+            double x = gamepad1.left_stick_x;
+            double y = -gamepad1.left_stick_y;
+            double a = -gamepad1.right_stick_x;
+
+            double x0 = x;
+            double y0 = y;
+            x = (Math.cos(-heading) * x0) - (Math.sin(-heading) * y0);
+            y = (Math.sin(-heading) * x0) + (Math.cos(-heading) * y0);
+
+            double[] p = new double[4];
+            p[0] = -x + y - a;
+            p[1] = x + y - a;
+            p[2] = -x + y + a;
+            p[3] = x + y + a;
+
+            double max = Math.max(1, Math.max(Math.abs(p[0]), Math.max(Math.abs(p[1]), Math.max(Math.abs(p[2]), Math.abs(p[3])))));
+            if (max > 1) for (int i = 0; i < 4; i++) p[i] /= max;
+            for (int i = 0; i < 4; i++) motors.get(i).setPower(p[i]);
+
             wheels[0] = motors.get(1).getCurrentPosition();
             wheels[1] = motors.get(2).getCurrentPosition();
             wheels[2] = motors.get(0).getCurrentPosition();
             wheels[3] = motors.get(3).getCurrentPosition();
 
-            double dfl = wheels[0] - prevWheels[0];
-            double dfr = wheels[1] - prevWheels[1];
-            double dbl = wheels[2] - prevWheels[2];
-            double dbr = wheels[3] - prevWheels[3];
-          
+            double dfl = (wheels[0] - prevWheels[0]) * IN_PER_TICK;
+            double dfr = (wheels[1] - prevWheels[1]) * IN_PER_TICK;
+            double dbl = (wheels[2] - prevWheels[2]) * IN_PER_TICK;
+            double dbr = (wheels[3] - prevWheels[3]) * IN_PER_TICK;
+
             double twistRobotY = (dfl + dfr + dbl + dbr) / 4;
             double twistRobotX = (dbl + dfr - dfl - dbr) / 4;
             double twistRobotTheta = heading - prevHeading;
@@ -78,15 +97,11 @@ public class BareBonesLocalizationOpMode extends LinearOpMode {
             prevWheels[3] = wheels[3];
             prevHeading = heading;
 
-            double[] twist = new double[]{twistRobotX * STRAFE_MULT, twistRobotY};
+            double[] twist = new double[]{twistRobotX * STRAFE_MULT, twistRobotY, twistRobotTheta};
 
             exp(curPose, twist);
             curPose[2] = heading;
 
-            curPose[0] += fieldTwist[0];
-            curPose[1] += fieldTwist[1];
-            curPose[2] = heading;
-            
             telemetry.addData("Position", "(%.2f, %.2f)", curPose[0], curPose[1]);
             telemetry.addData("Heading", "%.1f", Math.toDegrees(curPose[2]));
             telemetry.addData("Velocity", "(%.2f, %.2f)", twist[0], twist[1]);
@@ -95,7 +110,7 @@ public class BareBonesLocalizationOpMode extends LinearOpMode {
         }
     }
 
-    public static void exp(double cur[], double[] twist) {
+    public static void exp(double[] cur, double[] twist) {
         double dx = twist[0];
         double dy = twist[1];
         double dtheta = twist[2];
@@ -113,22 +128,15 @@ public class BareBonesLocalizationOpMode extends LinearOpMode {
             c = (1 - cosTheta) / dtheta;
         }
 
-        double magnitude = Math.hypot(cosTheta, sinTheta);
-        double sin = sinTheta / magnitude;
-        double cos = cosTheta / magnitude;
-        double rot = Math.atan2(sin, cos);
-
         double[] transform = new double[]{
                 dx * s - dy * c,
                 dx * c + dy * s,
-                rot
         };
-        
-        double curCos = Math.cos(cur[2]);
-        double curSin = Math.sin(cur[2]);
-        
-        cur[0] += transform[0] * curCos - transform[1] * curSin;
+
+        double curCos = Math.cos(-cur[2]);
+        double curSin = Math.sin(-cur[2]);
+
+        cur[0] -= transform[0] * curCos - transform[1] * curSin;
         cur[1] += transform[0] * curSin + transform[1] * curCos;
-        cur[2] += transform[2];
     }
 }
